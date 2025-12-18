96018dac5f722a3a182c3fbd1ce66e7812494340
dwy0r <dwyor.official@gmail.com>
2025-12-18 11:31:25 +0700

    Update data & ornamen, hapus gambar tidak terpakai

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -58,18 +58,6 @@ export const data = {
             id: 2,
             image: './src/assets/images/2.png'
         },
-        {
-            id: 3,
-            image: './src/assets/images/3.png'
-        },
-        {
-            id: 4,
-            image: './src/assets/images/4.png'
-        },
-        {
-            id: 5,
-            image: './src/assets/images/5.png'
-        }
     ],
 
     bank: [
